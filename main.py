ca7306b4413e44e1b4804b2a9576039f7f6c3bb7
marrmarie <mashusy2008@gmail.com>
2024-03-03 14:04:41 +0300

    прямоугольник, меняющий цвет при нажатии

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import pygame
+import pygame as pg
 
 WIDTH = 1300
 HEIGHT = 700
 SIZE = [WIDTH, HEIGHT]
+screen = pg.display.set_mode(SIZE)
+screen.fill(pg.Color('pink'))
+rect_big = pg.Rect(500, 250, 300, 200)
+pg.draw.rect(screen, ('white'), rect_big)
 
-screen = pygame.display.set_mode(SIZE)
-time = pygame.time.Clock()
+# surf = pg.Surface((150, 150))
+# surf.set_colorkey((0, 0, 0))
+# screen.blit(surf, pg.Rect(150, 150, 150, 150))
+active=False
+user_text= ' '
+running = True
+while running:
 
-while True:
-    screen.fill(pygame.Color('pink'))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-    pygame.display.flip()
-    time.tick(30)
+    events = pg.event.get()
+    for event in events:
+        if event.type == pg.QUIT:
+            running = False
+        if event.type == pg.MOUSEBUTTONDOWN:
+            print(pg.mouse.get_pos())
+            if rect_big.collidepoint(event.pos):
+                pg.draw.rect(screen, ('red'), rect_big)
+                #screen.blit(screen, pg.Rect())
+
+    pg.display.update()
+    pg.time.delay(100)
+
+pg.quit()
