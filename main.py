cea437d306c7da5ac2608bfdedef257a33747715
marrmarie <mashusy2008@gmail.com>
2024-03-03 11:36:20 +0300

    розовый экран

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print(0)
+import pygame
+
+WIDTH = 1300
+HEIGHT = 700
+SIZE = [WIDTH, HEIGHT]
+
+screen = pygame.display.set_mode(SIZE)
+time = pygame.time.Clock()
+
+while True:
+    screen.fill(pygame.Color('pink'))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    pygame.display.flip()
+    time.tick(30)
