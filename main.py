0002d6ee311fc32d3d2678563e541e81b0a18d10
marrmarie <mashusy2008@gmail.com>
2024-03-06 20:27:05 +0500

    лабиринт, где стена - препядствие

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ time = pg.time.Clock()
 x = 0
 y = 0
 
+lab = [[[] for i in range(columns)] for j in range(rows)]
 
 class Cell:
     def __init__(self, x, y):
@@ -39,15 +40,39 @@ class Cell:
         if self.visited:
             pg.draw.rect(screen, 'green', (x, y, SQUARE_SIZE, SQUARE_SIZE))
 
+        x1 = self.x // SQUARE_SIZE
+        y1 = self.y // SQUARE_SIZE
         if self.walls[0]:
             pg.draw.line(screen, 'black', [x, y], [x + SQUARE_SIZE, y], 3)
+        # else:
+        #     if 0 not in lab[x1][y1]:
+        #         lab[x1][y1].append(0)
+        #     if 2 not in lab[x1][y1 - 1]:
+        #         lab[x1][y1 - 1].append(2)
+
         if self.walls[1]:
             pg.draw.line(screen, 'black', [x + SQUARE_SIZE, y], [x + SQUARE_SIZE, y + SQUARE_SIZE], 3)
+        # else:
+        #     if 1 not in lab[x1][y1]:
+        #         lab[x1][y1].append(1)
+        #     if 3 not in lab[x1 + 1][y1]:
+        #         lab[x1 + 1][y1].append(3)
+
         if self.walls[2]:
             pg.draw.line(screen, 'black', [x + SQUARE_SIZE, y + SQUARE_SIZE], [x, y + SQUARE_SIZE], 3)
+        # else:
+        #     if 2 not in lab[x1][y1]:
+        #         lab[x1][y1].append(2)
+        #     if 0 not in lab[x1][y1 + 1]:
+        #         lab[x1][y1 + 1].append(0)
+
         if self.walls[3]:
             pg.draw.line(screen, 'black', [x, y + SQUARE_SIZE], [x, y], 3)
-
+        # else:
+        #     if 3 not in lab[x1][y1]:
+        #         lab[x1][y1].append(3)
+        #     if 1 not in lab[x1 - 1][y1]:
+        #         lab[x1 - 1][y1].append(1)
 
     def check(self, x, y):
         ind = lambda x, y: x + y * columns
@@ -76,22 +101,46 @@ class Cell:
 
 
 def passage(now, next):
+    global lab
     x1 = now.x - next.x
+    xx = now.x // SQUARE_SIZE
+    yy = now.y // SQUARE_SIZE
+
     if x1 == 1:
         now.walls[3] = False
         next.walls[1] = False
-    elif x1 == -1:
+    # else:
+    #     if 3 not in lab[xx][yy]:
+    #         lab[xx][yy].append(3)
+    #     if 1 not in lab[xx - 1][yy]:
+    #         lab[xx - 1][yy].append(1)
+
+    if x1 == -1:
         now.walls[1] = False
         next.walls[3] = False
+    # else:
+    #     if 1 not in lab[xx][yy]:
+    #         lab[xx][yy].append(1)
+    #     if 3 not in lab[xx + 1][yy]:
+    #         lab[xx + 1][yy].append(3)
+
     y1 = now.y - next.y
     if y1 == 1:
         now.walls[0] = False
         next.walls[2] = False
-    elif y1 == -1:
+    # else:
+    #     if 0 not in lab[xx][yy]:
+    #         lab[xx][yy].append(0)
+    #     if 2 not in lab[xx][yy - 1]:
+    #         lab[xx][yy - 1].append(2)
+    if y1 == -1:
         now.walls[2] = False
         next.walls[0] = False
-
-
+    # else:
+    #     if 2 not in lab[xx][yy]:
+    #         lab[xx][yy].append(2)
+    #     if 0 not in lab[xx][yy + 1]:
+    #         lab[xx][yy + 1].append(0)
 
 
 
@@ -104,8 +153,7 @@ cell_now = grid[0]
 
 queue = deque()
 running = True
-loc_x = 0
-loc_y = 0
+
 while running:
     screen.fill(pg.Color('pink'))
 
@@ -113,18 +161,26 @@ while running:
         if i.type == pg.QUIT:
             exit()
         elif i.type == pg.KEYDOWN:
-            if i.key == pg.K_LEFT and x - SQUARE_SIZE >= 0:
+            for j in range(len(lab)):
+                print(*lab[j])
+            q = grid[(x // SQUARE_SIZE) + (y // SQUARE_SIZE) * columns]
+            if i.key == pg.K_LEFT and x - SQUARE_SIZE >= 0 and not q.walls[3]:
                 x -= SQUARE_SIZE
-            elif i.key == pg.K_RIGHT and x + SQUARE_SIZE * 2 <= WIDTH:
+            elif i.key == pg.K_RIGHT and x + SQUARE_SIZE * 2 <= WIDTH and not q.walls[1]:
                 x += SQUARE_SIZE
-            elif i.key == pg.K_UP and y - SQUARE_SIZE >= 0:
+            elif i.key == pg.K_UP and y - SQUARE_SIZE >= 0 and not q.walls[0]:
                 y -= SQUARE_SIZE
-            elif i.key == pg.K_DOWN and y + SQUARE_SIZE * 2 <= HEIGHT:
+            elif i.key == pg.K_DOWN and y + SQUARE_SIZE * 2 <= HEIGHT and not q.walls[2]:
                 y += SQUARE_SIZE
+
+
+
     for c in grid:
         c.draw_lines_and_cell()
+
     cell_now.visited = True
     cell_now.fill_current()
+
     next_c = cell_now.go()
     if next_c:
         next_c.visited = True
